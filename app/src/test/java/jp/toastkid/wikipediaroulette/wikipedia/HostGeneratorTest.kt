e6d2f2340813c13069e2d559c9bf799a8e3e041c
Toast Kid <super_q1m@yahoo.co.jp>
2019-10-03 22:27:59 +0900

    feature #45 Implement Wikipedia host generator.

---
+++ app/src/test/java/jp/toastkid/wikipediaroulette/wikipedia/HostGeneratorTest.kt
@@ -0,0 +1,16 @@
+package jp.toastkid.wikipediaroulette.wikipedia
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.*
+
+/**
+ * @author toastkidjp
+ */
+class HostGeneratorTest {
+
+    @Test
+    fun test() {
+        assertEquals("https://ja.wikipedia.org/", HostGenerator().invoke(Locale.JAPAN))
+    }
+}
